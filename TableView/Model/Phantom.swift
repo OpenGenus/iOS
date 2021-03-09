1586ff243f2b1c20e7ec24096debdd20d71e824f
Sai Balaji <ksbalaji2000@outlook.com>
2021-03-09 17:14:36 +0530

    Added Custom Table View code along with app

---
+++ TableView/Model/Phantom.swift
@@ -0,0 +1,14 @@
+//
+//  Phantom.swift
+//  TableView
+//
+//  Created by Sai Balaji on 07/03/21.
+//
+
+import Foundation
+
+struct Phantom
+{
+    var name: String = ""
+    var imagename: String = ""
+}
